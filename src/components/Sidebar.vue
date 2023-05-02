5f16236b3f2daf180e45505534244a805bdb51b5
arjun.airtory <arjun12345krishna@gmail.com>
2023-05-03 02:58:48 +0530

    Sidebar active CSS

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -5,14 +5,15 @@
             <div class="flex overflow-y-auto flex-col flex-1 pt-5 pb-4">
                 <div class="flex-1 px-3 space-y-1 bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                     <ul class="pb-2 space-y-2">
+                        
                         <li>
-                            <a href="/" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700">
-                                <svg class="w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
+                            <a href="/" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg group dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" :class="{'bg-gray-100 dark:bg-gray-700': getCurrentUrl === '/'}">
+                                <svg class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
                                 <span class="ml-3" sidebar-toggle-item>Dashboard</span>
                             </a>
                         </li>
                         <li>
-                            <a href="/create" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group dark:text-gray-200 dark:hover:bg-gray-700">
+                            <a href="/create" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg group dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700" :class="{'bg-gray-100 dark:bg-gray-700': getCurrentUrl === '/create'}">
                                 <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path><path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd"></path></svg>
                                 <span class="flex-1 ml-3 whitespace-nowrap" sidebar-toggle-item>Create Survey</span>
                             </a>
@@ -23,6 +24,7 @@
                                 <span class="flex-1 ml-3 whitespace-nowrap" sidebar-toggle-item>Edit Survey</span>
                             </a>
                         </li>
+
                     </ul>
                 </div>
             </div>
@@ -32,3 +34,15 @@
     <div class="hidden fixed inset-0 z-10 bg-gray-900/50 dark:bg-gray-900/90" id="sidebarBackdrop"></div>
 
 </template>
+
+
+<script>
+
+    export default {
+        data() {
+            return {
+                getCurrentUrl: window.location.pathname,
+            }
+        },
+    }
+</script>
